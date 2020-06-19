ee5ce316a5e2661e330aeed98b0a3dc24bf39083
Johann CAHIER <johann.cahier@iot.bzh>
2020-06-19 20:16:44 +0200

    Improvements !
    - Header is now linkable from C++ / .ino files (thanks to extern " C" {...} directive)
    - 2 callbacks added :
        - 1 is to detect a Slave Receive Start Condition (master wants to transmit data to us)
        - 1 to detect the STOP Condition associatied (Master is done with talking)
    - FIXED THE "request done" CONDITION ! For slave transmit mode, the request callback now
      receives the 3 states : INITIAL, CONTINUED, and DONE.

--- I2CSlave.c
+++ I2CSlave.c
@@ -5,11 +5,19 @@
 
 static void (*i2c_slave_recv)(uint8_t);
 static void (*i2c_slave_req)(request_t);
+static void (*i2c_slave_receive_start)(void);
+static void (*i2c_slave_receive_stop)(void);
 
-void i2c_slave_setCallbacks(void (*recv)(uint8_t), void (*req)(request_t))
+
+void i2c_slave_setCallbacks(void (*start)(void),
+                            void (*recv)(uint8_t),
+                            void (*stop)(void),
+                            void (*req)(request_t))
 {
   i2c_slave_recv = recv;
   i2c_slave_req = req;
+  i2c_slave_receive_start = start;
+  i2c_slave_receive_stop = stop;
 }
 
 void i2c_slave_init(uint8_t address)
@@ -35,24 +43,31 @@ ISR(TWI_vect)
 {
   switch(TW_STATUS)
   {
+    case TW_SR_SLA_ACK:
+      if (i2c_slave_receive_start) i2c_slave_receive_start();
+      break;
+    case TW_SR_STOP:
+      if (i2c_slave_receive_stop) i2c_slave_receive_stop();
+      break;
     case TW_SR_DATA_ACK: // data received, ACK returned
       // received data from master, call the receive callback
-      i2c_slave_recv(TWDR);
+      if (i2c_slave_recv) i2c_slave_recv(TWDR);
       break;
     case TW_ST_SLA_ACK: // SLA+R
       // master is requesting data, call the request callback
-      i2c_slave_req(INITIAL);
+      if (i2c_slave_req) i2c_slave_req(INITIAL);
       break;
     case TW_ST_DATA_ACK: // data transmitted, ACK received
       // master is requesting data, call the request callback
-      i2c_slave_req(CONTINUATION);
+      if (i2c_slave_req) i2c_slave_req(CONTINUATION);
       break;
-    /*case TW_ST_LAST_DATA: // last data byte transmitted, (n)ACK received
-      i2c_slave_req(DONE);
-      break;*/
+    case TW_ST_DATA_NACK: // last data byte transmitted, (n)ACK received
+      if (i2c_slave_req) i2c_slave_req(DONE);
+      break;
     case TW_BUS_ERROR:
       // some sort of erroneous state, prepare TWI to be readdressed
       TWCR = 0;
+      // TODO : is this enough ?
       break;
     default:
       break;
--- I2CSlave.h
+++ I2CSlave.h
@@ -4,8 +4,11 @@
 #include <avr/interrupt.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-// used as argument to call request callback :
+    // used as argument to call request callback :
 typedef enum {
     INITIAL = 1,      // when master open communication for reading
     CONTINUATION = 2, // when master ACKs transmitted data (it expects continuation)
@@ -14,17 +17,23 @@ typedef enum {
 
 void i2c_slave_init(uint8_t address);
 void i2c_slave_stop(void);
-void i2c_slave_setCallbacks(void (*recv)(uint8_t), void (*req)(request_t));
+void i2c_slave_setCallbacks(void (*start)(void),      // callback for Slave Receive START Condition
+                            void (*recv)(uint8_t),    // callback for ACKed Slave Receive Data
+                            void (*stop)(void),       // callback for Slave Receive STOP Condition
+                            void (*req)(request_t)    // callback for Slave Transmit (init, cont, done)
+                           );
 
-// last should be 0 in normal operation, execpt for the LAST byte transmitted by the slave
+// last should be 0 in normal operation, except for the LAST byte transmitted by the slave
 inline void __attribute__((always_inline)) i2c_slave_transmitByte(uint8_t data/*, uint8_t last*/)
 {
-//     if (last) {
-//         TWCR &= ~(1 << TWEA);
-//     }
     TWDR = data;
 }
 
 ISR(TWI_vect);
+
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif
